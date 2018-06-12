79f76b11fdfda30d0e12377d934423d1ed42635e
Carson Kelley <carsonmkelley@gmail.com>
2018-06-12 17:49:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+function nowServing() {
+  if(array.length > 0) {
+    return array.shift()
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
