f864b1979adf428cda16290779e364e7ac913ed9
Carson Kelley <carsonmkelley@gmail.com>
2018-06-12 17:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
-function nowServing() {
+function nowServing(line) {
   if(array.length > 0) {
     return array.shift()
   } else {
