8a683c57c856cfc614f9ddbea8754dc313cfae6c
Carson Kelley <carsonmkelley@gmail.com>
2018-06-12 17:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function currentLine(line) {
   } else {
     for(var i = 0; i < line.length; i++) {
       result += `${i+1}. ${line[i]}`
+      if(i !== line.length - 1){
+        result += ","
+      }
     }
   }
 }
