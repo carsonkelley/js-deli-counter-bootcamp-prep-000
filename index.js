635cc757583ca38a475e352211bc8d3add2e196c
Carson Kelley <carsonmkelley@gmail.com>
2018-06-12 17:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
     for(var i = 0; i < line.length; i++) {
       result += `${i+1}. ${line[i]}`
       if(i !== line.length - 1){
-        result += ","
+        result += ", "
       }
     }
     return result
